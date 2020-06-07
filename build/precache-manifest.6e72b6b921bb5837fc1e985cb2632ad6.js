3b442de71eb8452a5f154ffcb6b86341a7c42730
abinash013 <45894844+abinash013@users.noreply.github.com>
2020-06-07 15:41:50 +0530

    todolist

---
+++ build/precache-manifest.6e72b6b921bb5837fc1e985cb2632ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18522447567fcee505d8f70ac6841a97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ca098f179f7cb2daabe",
+    "url": "/static/css/main.8fb69d37.chunk.css"
+  },
+  {
+    "revision": "e2d78d059b6db8cb186b",
+    "url": "/static/js/2.74829db0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.74829db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca098f179f7cb2daabe",
+    "url": "/static/js/main.30c3a219.chunk.js"
+  },
+  {
+    "revision": "e980f59a9a376501375f",
+    "url": "/static/js/runtime-main.def2087a.js"
+  }
+]);
